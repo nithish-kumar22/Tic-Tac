51a19a7dd33f37223b924d069ca6f3d18e834823
AKJVE <45726858+AKJVE@users.noreply.github.com>
2018-12-09 11:33:37 +0530

    Update game.c

--- game.c
+++ game.c
@@ -2,20 +2,58 @@
 #include <windows.h>
 #include <stdlib.h>
 #include <string.h>
-int main()
+#include <conio.h>
+void main()
 {
+	system("C:\\Documents\\mkdir TIC_TAC");
+	FILE *fp;
+	fp = fopen("C:\\Documents\\TIC_TAC\\tic-tac_user.txt","w");
+	MessageBox(0,"WELCOME to this game... This game tells about your aim","TIC-TAC",1+64);
+	system("cd documents");
 	system("cls");
 	system("color 5f");
 	int a,c,d,e,f;
 	int num;
-	char *passw;
+	char passw[10];
 	int status;
-	char b[30];
+	char b[100];
+	printf("\t\t\t\t\tWELCOME TO THIS GAME!\n");
+	system("pause");
+	Beep(100,100);
+    Beep(200,200);
+    Beep(300,300);
+    Beep(400,400);
+    Beep(500,500);
+    Beep(400,600);
+    Beep(300,700);
+    Beep(500,800);
+    Beep(400,700);
+    Beep(400,300);
+    Beep(300,200);
+    Beep(300,300);
+    Beep(400,400);
+    Beep(500,500);
+    Beep(600,600);
+    Beep(500,600);
+    Beep(800,800);
+    Beep(700,700);
+    Beep(300,300);
 	printf("Enter the game password:");
 	gets(passw);
-	if(strcmp(passw,"akash") == 0)
+	if(strcmp(passw,"hsaka") == 0)
 	{
-		printf("correct");
+		Beep(400,500);
+		Beep(750,800);
+		printf("Password is correct!!\n");
+		MessageBox(0,"That is a correct password","Password",1+64);
+	}
+	else
+	{
+		Beep(400,500);
+		Beep(750,800);
+		printf("Incorrect Password~~");
+		MessageBox(0,"That is an Incorrect password... Please try again~","Password",1+64);
+		exit(0);
 	}
 	printf("Select 0 to exit and 1 to continue..");
 	scanf("%d",&num);
@@ -27,8 +65,10 @@ int main()
 	{
 	printf("This Game Tell about Your Aim!!");
 	printf("\nEnter Your Name:");
-	scanf("%s",&b);
-	printf("WELCOME %s!",b);
+	gets(b);
+	printf("Welcome ");
+	puts(b);
+	printf("!");
 	printf("\n1 = Green \n2 = Red \n3 = Blue \n4 = White");
 	printf("\nPlease Select a Colour:");
 	scanf("%d",&c);
@@ -38,7 +78,7 @@ int main()
 	{
 		system("color 2f");
 		printf("You Selected Green Colour!");
-		printf("\n1 \n3 \n5 \n7");
+		printf("\n\t1 \n\t\t3 \n\t\t\t5 \n\t\t\t\t7");
 		printf("\nPlease Select a Number:");
 		scanf("%d",&a);
 		if(a == 1)
@@ -55,9 +95,9 @@ int main()
 	{
 		system("color 4f");
 		printf("You Selected Red Colour!");
-		printf("\n2 \n4 \n6 \n8");
+		printf("\n\t2 \n\t\t4 \n\t\t\t6 \n\t\t\t\t8");
 		printf("\nPlease Select a Number:");
-		scanf("%d",&d);
+		scanf("%d", &d);
 		if(d == 2)
 			printf("Your Future is Chief Minister#!");
 		if(d == 4)
@@ -73,7 +113,7 @@ int main()
 	{
 		system("color 1f");
 		printf("You Selected Blue Colour!");
-		printf("\n3 \n6 \n9 \n7");
+		printf("\n\t3 \n\t\t6 \n\t\t\t9 \n\t\t\t\t7");
 		printf("\nPlease Select a Number:");
 		scanf("%d",&e);
 		if(e == 3)
@@ -91,7 +131,7 @@ int main()
 	{
 		system("color 7f");
 		printf("You Selected White Colour!");
-		printf("\n1 \n5 \n8 \n3");
+		printf("\n\t1 \n\t\t5 \n\t\t\t8 \n\t\t\t\t3");
 		printf("\nPlease Select a Number:");
 		scanf("%d",&f);
 		if(f == 1)
@@ -107,5 +147,6 @@ int main()
 	}
 }
 }
-	return 0;
+	getch();
 }
+
